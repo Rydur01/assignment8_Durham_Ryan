eb22fba13778c5c5f04d61c12c3799f2189a7c1a
Rydur01 <117382074+Rydur01@users.noreply.github.com>
2022-11-07 00:54:32 -0500

    Add files via upload

---
+++ WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment8_Durham_Ryan.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Ryan Durham"
+        };
+        [HttpGet]
+        public string[] Get()
+        {
+            return Summaries;
+        }
+    }
+}
